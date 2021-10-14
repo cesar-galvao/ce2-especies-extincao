241952215f254ffd2a008903f0c948547b3c17dc
cesar-galvao <cesaraugusto.galvao@gmail.com>
2021-10-14 20:14:45 -0300

    atualizacao shiny

--- shiny/app-especies-extincao.R
+++ shiny/app-especies-extincao.R
@@ -2,8 +2,12 @@ library(shinydashboard)
 library(shiny)
 library(tidyverse)
 
-data <- read.csv("./data/especies_2020_pronto.csv")
+
+data <- readRDS("../data/especies_2020_pronto.RDS")
 # inserir série histórica?
+
+#input de lista
+#página com endereço reactive
 
 #triggers de avisos para entrada em cada aba. Como notificar cada vez que abre uma aba nova?
 
@@ -21,7 +25,10 @@ sidebar <- dashboardSidebar(
     menuItem("Apresentação", tabName = "apresentacao"),
     menuItem("Mapa", tabName = "mapa"),
     menuItem("Comparações", tabName = "testes-hip"), #testes de hipotese
-    menuItem("Busca de espécie", tabName = "wiki"),
+    menuItem("Busca de espécie", tabName = "wiki",
+             selectInput("filtro1", ) #selecao dos filtros
+             
+    ),
     menuItem("Banco de dados", tabName = "database") #dizer de onde vem, mostrar em tabela
   )
 )
@@ -34,6 +41,10 @@ body <- dashboardBody(
     tabItem(tabName = "testes-hip"),
     tabItem(tabName = "wiki", #incluir filtro para grupo, categoria ameaca, bioma em caixa superior
             htmlOutput("wiki_page")),
+    tabItem(tabName = "wiki",
+            #Caixa com menu dos filtros para grupo, categoria ameaca, bioma em caixa superior
+            htmlOutput("wiki_page")
+    ),
     tabItem(tabName = "database")
   )
 )
@@ -47,6 +58,21 @@ ui <- dashboardPage(header = header,
 
 server <- function(input, output) {
   
+  # processamento dos filtros
+  
+  #gera html pra usar no getPage
+  
+  #gerar link com base nos inputs
+  getPage<-function() {
+    return(tags$iframe(src = "https://en.wikipedia.org/wiki/Cognitive_bias"
+                       , style="width:100%;",  frameborder="0"
+                       ,id="iframe"
+                       , height = "500px"))
+  }
+  output$wiki_page<-renderUI({
+    x <- input$test
+    getPage()
+  })
 }
 
 shinyApp(ui, server)
